6d4b21f7ce1ca799c91c982d6b5d81d166f8cf6f
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-11-29 21:57:10 -0800

    got images and blurbs to display on page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -3,8 +3,12 @@ import './Animal.css'
 
 export default function Animal(props) {
   return (
-    <div className="animal">
-      <p className="name">{props.name}</p>
-    </div>
+    <>
+      <div className="animal" style={{ top: props.top, left: props.left }}>
+        <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+        <span className="name">{props.name}</span>
+        <span>{props.says}</span>
+      </div>
+    </>
   )
 }
